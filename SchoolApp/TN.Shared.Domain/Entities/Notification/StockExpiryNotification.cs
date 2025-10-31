e062acf6d0efa277c42d858788dd81f6ccc80af0
Developer-Suman <officegit111@gmail.com>
2025-10-31 11:04:33 +0545

    Succeed: Configuration of SignalR and getALlNotification Queries

---
+++ SchoolApp/TN.Shared.Domain/Entities/Notification/StockExpiryNotification.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TN.Shared.Domain.Primitive;
+
+namespace TN.Shared.Domain.Entities.Notification
+{
+    public class StockExpiryNotification : Entity
+    {
+        public StockExpiryNotification() : base(null)
+        {
+
+        }
+
+        public StockExpiryNotification(
+            string id,
+            DateTime? expiredDate,
+        string? itemName,
+        string? schoolId,
+        int? daysToExpire,
+        string? description,
+        string? stockCenterId
+            ) : base(id)
+        {
+            ExpiredDate = expiredDate;
+            ItemName = itemName;
+            SchoolId = schoolId;
+            DaysToExpire = daysToExpire;
+            Description = description;
+            StockCenterId = stockCenterId;
+
+        }
+        public DateTime? ExpiredDate { get; set; }
+        public string? ItemName { get; set; }
+        public string? SchoolId { get; set; }
+        public int? DaysToExpire { get; set; }
+        public string? Description { get; set; }
+
+        public string? StockCenterId { get; set; }
+
+    }
+}
